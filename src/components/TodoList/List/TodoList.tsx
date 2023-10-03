a09fa20ef1ab71fe0cfaf28d34cc55ca9e5e297b
Paltrow <alexandriys2@ukr.net>
2023-10-03 11:40:49 +0300

    fix: Changed TodoItem to be able to accept todo prop as one property. Changed todoReducer,setTodoDone logic to fit the memoization. Separated TodoItem text_crossed style into two classes.

--- src/components/TodoList/List/TodoList.tsx
+++ src/components/TodoList/List/TodoList.tsx
@@ -2,8 +2,6 @@ import { FC } from 'react';
 
 import { useAppSelector } from '@redux';
 
-import { useTodoTask } from '@hooks';
-
 import { TodoItem } from '../Item';
 
 import style from './TodoList.module.scss';
@@ -11,23 +9,14 @@ import style from './TodoList.module.scss';
 export const TodoList: FC = () => {
   const { todoList } = useAppSelector((state) => state.todo);
 
-  const { onSetDone } = useTodoTask();
-
   if (!todoList.length) return null;
 
   return (
     <section className={style.container}>
       <ul className={style.list}>
-        {todoList.map(({ id, text, isDone, creationDate, expirationDate }) => (
-          <li key={id}>
-            <TodoItem
-              id={id}
-              text={text}
-              isDone={isDone}
-              creationDate={creationDate}
-              expirationDate={expirationDate}
-              onSetDone={onSetDone}
-            />
+        {todoList.map((todoItem) => (
+          <li key={todoItem.id}>
+            <TodoItem todo={todoItem} />
           </li>
         ))}
       </ul>
